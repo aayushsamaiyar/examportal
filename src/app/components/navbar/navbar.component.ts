fd09964c42217636ca27f96e6d4605733c001b4a
aayushsamaiyar <aayushsamaiyar@gmail.com>
2022-12-03 14:26:56 +0530

    login done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit{
   
-  constructor() { }
+  constructor(public login: LoginService) { }
+
+  public logout(){
+    this.login.logout();
+    window.location.reload();
+  }
 
   ngOnInit(): void {
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+
+  loginData={
+    username:'',
+    password:''
+  };
+
+  constructor(private snack: MatSnackBar, private login: LoginService){
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  formSubmit(){
+    console.log("ogin button clicked");
+
+    if(this.loginData.username.trim() == '' || this.loginData.username == null){
+      this.snack.open("username is required !!",'ok',{duration:3000});
+      return;
+    }
+
+    if(this.loginData.password.trim() == '' || this.loginData.password == null){
+      this.snack.open("password is required !!",'ok',{duration:3000});
+      return;
+    }
+
+    // requesting server for token generation
+
+    this.login.generateToken(this.loginData).subscribe(
+      (data:any) => {
+        console.log("Success");
+        console.log(data);
+
+        // login 
+
+        this.login.loginuser(data.token);
+        this.login.getCurrentuser().subscribe(
+          (user:any)=>{
+            this.login.setUser(user);
+            console.log(user);
+
+            if(this.login.getUserRole()=="admin"){
+              window.location.href='/admin'
+            }else if(this.login.getUserRole()=="NORMAL"){
+              window.location.href='/user-dashboard'
+            }else{
+              this.login.logout();
+            }
+
+
+
+          },
+          (error)=>{
+            console.log("error in login")
+            this.snack.open("Invalid Details! Try again",'ok',{
+              duration:3000,
+            })
+          }
+        )
+
+      },
+      (error)=>{
+        console.log("Error !!");
+        console.log(error);
+
+      }
+    )
+
+  }
+  
 }
